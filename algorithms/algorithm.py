18b402753cc8eeba5ae10ddd8dfd77a1d24a87a5
IvonLuiz <ivonluizgarcia@gmail.com>
2023-11-04 11:20:48 -0300

    Padrão Command

--- algorithms/algorithm.py
+++ algorithms/algorithm.py
@@ -1,8 +1,9 @@
 class Algorithm:
-
+    """This class follows command pattern"""
     def __init__(self, processes=[]):
         """Interface for scheduling algorithms"""
         self.processes = processes
 
-    def execute(self):
-        pass
+    def execute(self) -> None:
+        """Abstract method"""
+        raise NotImplementedError("No execution implemented")
--- system.py
+++ system.py
@@ -1,9 +1,15 @@
 from process import Process
+from algorithms.fifo import Fifo
+from algorithms.sjf import SJF
+from algorithms.round_robin import RoundRobin
+from algorithms.edf import EDF
+from algorithms.algorithm import Algorithm
 
 class System:
-    def __init__(self, overhead):
+    def __init__(self, overhead, quantum):
         self.processes = []
         self.overhead = overhead
+        self.quantum = quantum
         
     def add_process(self, arrival_time, exec_time, deadline, priority):
         process_id = len(self.processes) + 1
@@ -15,3 +21,7 @@ class System:
 
         process.system_overhead = self.overhead
         self.processes.append(process)
+        
+    def exec_algorithm(self, algorithm: Algorithm):
+        algorithm_scheduler = algorithm(self.processes)
+        algorithm_scheduler.execute()
