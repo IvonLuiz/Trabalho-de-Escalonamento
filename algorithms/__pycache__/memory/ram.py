0f8e3bcb57bb1336cd795267c88685c2f9f996ec
Guilherme Carvalho Caria <amuica379@gmail.com>
2023-11-09 23:53:12 -0300

    Created Disk and Ram

---
+++ algorithms/__pycache__/memory/ram.py
@@ -0,0 +1,12 @@
+class Ram:
+    def __init__(self, storage, storageLeft) -> None:
+        self.storage= [0]*50
+        self.storageLeft= 50
+
+    def storeItem(self, processId, overrideId, numberOfPages):
+
+        return pagesWritten 
+
+
+
+    def removeItem(self, processId):
