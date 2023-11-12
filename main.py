b26016442111a371621f32537a5dadc03f732920
IvonLuiz <ivonluizgarcia@gmail.com>
2023-11-12 18:17:33 -0300

    main

--- main.py
+++ main.py
@@ -4,12 +4,24 @@ from algorithms.sjf import SJF
 from algorithms.round_robin import RoundRobin
 from algorithms.edf import EDF
 
-system = System(overhead=0, quantum=2)
+system = System(overhead=1, quantum=2)
 
-system.add_process(0, 10, 5, 2)
-system.add_process(3, 8, 2, 1)
-system.add_process(6, 12, 1, 3)
+# system.add_process(arrival_time=0, exec_time=4, deadline=7, priority=2)
+# system.add_process(arrival_time=2, exec_time=2, deadline=5, priority=2)
+# system.add_process(arrival_time=4, exec_time=1, deadline=8, priority=2)
+# system.add_process(arrival_time=6, exec_time=3, deadline=10, priority=2)
 
-system.exec_algorithm(Fifo)
-system.exec_algorithm(SJF)
+system.add_process(arrival_time=0, exec_time=5, deadline=7, priority=2)
+system.add_process(arrival_time=3, exec_time=3, deadline=5, priority=2)
+system.add_process(arrival_time=7, exec_time=10, deadline=8, priority=2)
+system.add_process(arrival_time=8, exec_time=2, deadline=10, priority=2)
+system.add_process(arrival_time=10, exec_time=4, deadline=10, priority=2)
+
+# system.add_process(arrival_time=0, exec_time=5, deadline=7, priority=2)
+# system.add_process(arrival_time=1, exec_time=2, deadline=5, priority=2)
+# system.add_process(arrival_time=12, exec_time=7, deadline=8, priority=2)
+
+# system.exec_algorithm(Fifo)
+# system.exec_algorithm(SJF)
+# system.exec_algorithm(RoundRobin)
 system.exec_algorithm(EDF)
