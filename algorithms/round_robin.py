d300d834cc63e1c4bce6c6af5de1cacfe26cb6a2
Felipe Freire <53775377+Felipe-Freire@users.noreply.github.com>
2023-11-12 19:21:24 -0300

    Update rr
    retorna dicionário com id e lista de execução.

--- algorithms/round_robin.py
+++ algorithms/round_robin.py
@@ -1,5 +1,7 @@
 from queue import Queue
 from algorithms.algorithm import Algorithm
+from process import Process
+
 
 class RoundRobin(Algorithm):
 
@@ -12,43 +14,44 @@ class RoundRobin(Algorithm):
     def execute(self):
         time = 0
         current_process = None
+        execution_intervals = {}  # Dicionário para armazenar os intervalos de execução de cada processo
+
         # Case process enters late
         self.__verify_late_arrival(time)
 
         while True:
-            current_process = self.process_queue.get()            
-                            
+            current_process = self.process_queue.get()
+
             if current_process.execution_time <= self.quantum:
                 # The process is completed within the current quantum
+                execution_intervals.setdefault(current_process.id, []).append((time, time + current_process.execution_time))
                 time += current_process.execution_time
                 current_process.execution_time = 0
-                print(f"Process {current_process.id} executed. Finish time: {time}")
                 self.__verify_arrival_while_processing(time)
             else:
                 # The process still has time remaining after the quantum
+                execution_intervals.setdefault(current_process.id, []).append((time, time + self.quantum))
                 time += self.quantum + self.overhead
                 current_process.reduce_exec_time(self.quantum)
-                print(f"Process {current_process.id} executed for {self.quantum} units. "
-                f"Time to finish: {current_process.execution_time}. "
-                f"Time: {time}")
-                
+
                 self.__verify_arrival_while_processing(time)
                 self.process_queue.put(current_process)
-        
+
             # Case process enters late
             time = self.__verify_late_arrival(time)
-            
+
             if self.process_queue.empty():
                 break
 
+        return execution_intervals
 
     def __verify_arrival_while_processing(self, time):
         to_remove = []
         for i, proc in enumerate(self.processes):
             if proc.arrival_time <= time:
                 self.process_queue.put(proc)
-                to_remove.append(i)  
-                
+                to_remove.append(i)
+
         for index in reversed(to_remove):
             self.processes.pop(index)
 
@@ -59,3 +62,20 @@ class RoundRobin(Algorithm):
             self.processes.pop(0)
             time = proc.arrival_time
         return time
+
+
+if __name__ == "__main__":
+    processes = [
+        Process(id=1, exec_time=5, priority=1, deadline=10, arrival_time=0),
+        Process(id=2, exec_time=3, priority=2, deadline=8, arrival_time=1),
+        Process(id=3, exec_time=7, priority=3, deadline=15, arrival_time=2),
+    ]
+
+    rr = RoundRobin(processes)
+    execution_intervals = rr.execute()
+
+    print(execution_intervals)
+
+    print("Execution Intervals:")
+    for process_id, interval in execution_intervals.items():
+        print(f"Process {process_id}: {interval}")
