22938ea09ff76a2a469cb661e1074f277acc5d21
mochimila <camilaalmeida47@gmail.com>
2023-11-12 17:52:43 -0300

    removed process list

--- mmu.py
+++ mmu.py
@@ -2,17 +2,6 @@ from memory.ramMemory import Ram
 from memory.diskMemory import Disk
 from process import Process
 
-
-process= []
-#id, exec_time, priority, deadline, number_of_pages=5, arrival_time=0
-process.append(Process(1, 0, 10, 5, 20, 0))
-process.append(Process(2, 3, 8, 2, 13, 0))
-process.append(Process(3, 6, 12, 1, 15, 0))
-process.append(Process(4, 0, 10, 5, 10, 0))
-process.append(Process(9, 0, 10, 5, 43, 0))
-
-
-
 class MemoryManagementUnit:
     def __init__(self, algorithm, processList) -> None:
         self.ram= Ram(50,50)
