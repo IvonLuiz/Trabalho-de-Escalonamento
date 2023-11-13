4c935266cec160cb953c5637b596a8750767d23f
IvonLuiz <ivonluizgarcia@gmail.com>
2023-11-13 00:16:30 -0300

    setando prioridade default

--- process.py
+++ process.py
@@ -1,6 +1,6 @@
 class Process:
 
-    def __init__(self, id, exec_time, priority, deadline, arrival_time=0) -> None:
+    def __init__(self, id, exec_time, deadline, arrival_time=0, priority=0) -> None:
         self.id = id
         self.arrival_time = arrival_time
         self.execution_time = exec_time
