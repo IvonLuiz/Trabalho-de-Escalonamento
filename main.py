c8208f37207b7cfce463a6735757e85244bbec88
IvonLuiz <ivonluizgarcia@gmail.com>
2023-11-12 21:09:41 -0300

    main

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ from csv_reader import CSVReader
 CSV_FILE = 'csv/input_file.csv'
 processes = CSVReader(CSV_FILE).get_processes()
 
-system = System(overhead=0, quantum=2)
+system = System(processes, overhead=0, quantum=2)
 
-system.add_process(0, 10, 5, 2)
-system.add_process(3, 8, 2, 1)
-system.add_process(6, 12, 1, 3)
+# system.add_process(0, 10, 5, 2)
+# system.add_process(3, 8, 2, 1)
+# system.add_process(6, 12, 1, 3)
 
-system.exec_algorithm(Fifo)
+#system.exec_algorithm(Fifo)
 system.exec_algorithm(SJF)
-system.exec_algorithm(EDF)
+#system.exec_algorithm(EDF)
+print(system.get_next_execution_interval())
+print(system.get_next_execution_interval())
+print(system.get_next_execution_interval())
+print(system.get_next_execution_interval())
+print(system.current_time)
