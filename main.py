7246642bd2136decd1f07c21024c18e7ae54b803
IvonLuiz <ivonluizgarcia@gmail.com>
2023-11-13 00:16:17 -0300

    main teste

--- main.py
+++ main.py
@@ -2,22 +2,60 @@ from system import System
 from algorithms.fifo import Fifo
 from algorithms.sjf import SJF
 from algorithms.edf import EDF
+from algorithms.round_robin import RoundRobin
 from csv_reader import CSVReader
+from process import Process
 
-CSV_FILE = 'csv/input_file.csv'
-processes = CSVReader(CSV_FILE).get_processes()
+# processes = [
+#     Process(id=1, exec_time=5, priority=1, deadline=10, arrival_time=0),
+#     Process(id=2, exec_time=3, priority=2, deadline=8, arrival_time=1),
+#     Process(id=3, exec_time=7, priority=3, deadline=30, arrival_time=2),
+# ]
 
-system = System(processes, overhead=0, quantum=2)
+# rr = RoundRobin(processes)
+# execution_intervals, overload_intervals = rr.execute()
 
-# system.add_process(0, 10, 5, 2)
-# system.add_process(3, 8, 2, 1)
-# system.add_process(6, 12, 1, 3)
+# print(execution_intervals)
+# print(overload_intervals)
 
-#system.exec_algorithm(Fifo)
-system.exec_algorithm(SJF)
-#system.exec_algorithm(EDF)
-print(system.get_next_execution_interval())
-print(system.get_next_execution_interval())
-print(system.get_next_execution_interval())
-print(system.get_next_execution_interval())
-print(system.current_time)
+# print("Execution Intervals:")
+
+
+# for process_id, interval in execution_intervals.items():
+#     print(f"Process {process_id}: {interval}")
+    
+
+# if __name__ == "__main__":
+#     processes = [
+#         Process(id=1, exec_time=5, priority=1, deadline=10, arrival_time=0),
+#         Process(id=2, exec_time=3, priority=2, deadline=8, arrival_time=1),
+#         Process(id=3, exec_time=7, priority=3, deadline=10, arrival_time=2),
+#     ]
+
+#     edf = EDF(processes)
+#     execution_intervals, overload_intervals = edf.execute()
+
+#     print("Execution Intervals:")
+#     for process_id, interval in execution_intervals.items():
+#         print(f"Process {process_id}: {interval}")
+#     print("Overload Intervals:")
+#     for process_id, overloads in overload_intervals.items():
+#         print(f"Process {process_id}: {overloads}")
+
+# Example usage
+if __name__ == "__main__":
+    processes = [
+        Process(id=1, exec_time=5, priority=1, deadline=10, arrival_time=0),
+        Process(id=2, exec_time=3, priority=2, deadline=8, arrival_time=1),
+        Process(id=3, exec_time=7, priority=3, deadline=15, arrival_time=2),
+    ]
+
+    fifo_scheduler = Fifo(processes)
+    execution_intervals, overload_intervals = fifo_scheduler.execute()
+
+    print(execution_intervals)
+    print(overload_intervals)
+    
+    print("Execution Intervals:")
+    for process_id, interval in execution_intervals.items():
+        print(f"Process {process_id}: {interval}")
