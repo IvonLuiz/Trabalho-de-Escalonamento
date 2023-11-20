5289ddb26b49d4a9d8f8f045bbcfbac3edf62a04
Guilherme Carvalho Caria <amuica379@gmail.com>
2023-11-20 13:05:11 -0300

    Updated API

--- api_flask.py
+++ api_flask.py
@@ -3,11 +3,13 @@ from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 from system import System  # Make sure 'System' is available in the same directory
 import json
+from process import Process
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 system_instance = System()
+
 
 processes_list_ram = []
 processes_list_disk = []
@@ -17,29 +19,36 @@ average_turnaround = 0
 # Route to initialize the system with received data
 @socketio.on('start')
 def process_data(data_json):
-    print("entrei no start")
-    print(data_json)
+    processList= []
     data = json.loads(data_json)
-    print(data['delay'])
-    # # Extract information from received data
-    # processes_list = data.get('process', [])
-    # scheduling_algorithm = data.get('cpuAlgorithm', '')
-    # paging_algorithm = data.get('memoryAlgorithm', '')
-    # quantum = data.get('quantum', 0)
-    # overhead = data.get('overHead', 0)
-    # delay = data.get('delay', 0)
-    #
     # # Configure the system with received data
-    # system_instance.set_processes_list(processes_list)
-    # system_instance.set_quantum(quantum)
-    # system_instance.set_overhead(overhead)
-    # system_instance.set_delay(delay)
-    #
+    for i in data['process']:
+        currentProcess= Process(
+            int(i['id']), 
+            int(i['execTime']),
+            int(i['deadline']),
+            int(i['numberOfPages']),
+            int(i['arrivalTime']),    
+        )
+        processList.append(currentProcess)
+    system_instance.set_quantum(int(data['quantum']))
+    system_instance.set_overhead(int(data['overHead']))
+    system_instance.set_delay(int(data['delay']))
+    system_instance.set_processes_list(processList)
+    
     # # Execute the defined algorithms
-    # system_instance.exec_algorithm(scheduling_algorithm, paging_algorithm)
+    system_instance.exec_algorithm(data['cpuAlgorithm'], data['memoryAlgorithm'])
+
+    #Initial emit with disk, ram and empty gantt matrix
+    socketio.emit('diskTest', {'disk': system_instance.memory.disk.storage})
+
+    #Execute each CPU cycle in a while loop
+    system_instance.process_execution()
+    
 
     # Emit success status to the client
-    socketio.emit('update', {'status': 'success', 'message': 'Data received successfully.'})
+    #socketio.emit('update', {'status': 'success', 'message': 'Data received successfully.'})
+    socketio.emit('diskTest', {'disk': system_instance.memory.disk.storage})
 
 
 # Route to reset the system instance
@@ -58,7 +67,6 @@ def send_update():
 def handle_connect():
     # Send initial data when a WebSocket client connects
     print("conectado")
-    emit('update', {'processes_list_ram': processes_list_ram, 'processes_list_disk': processes_list_disk, 'gantt_matrix': gantt_matrix})
 
 if __name__ == "__main__":
     socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
--- process.py
+++ process.py
@@ -1,11 +1,10 @@
 class Process:
 
-    def __init__(self, id, exec_time, priority, deadline, number_of_pages=5, arrival_time=0) -> None:
+    def __init__(self, id, exec_time, deadline, number_of_pages, arrival_time) -> None:
         self.id = id
         self.arrival_time = arrival_time
         self.execution_time = exec_time
         self.deadline = deadline
-        self.priority = priority
         self.number_of_pages = number_of_pages
 
     def reduce_exec_time(self, time_reduce):
